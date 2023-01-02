0fbdc22b7f3ca594b6cdebd66784af6a4903cb6a
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-01-02 17:51:38 +0300

    music players controls added

--- music-player/app.js
+++ music-player/app.js
@@ -2,8 +2,8 @@ const container = document.querySelector(".container");
 const image = document.querySelector("#music-image");
 const title = document.querySelector("#music-details .title");
 const singer = document.querySelector("#music-details .singer");
-const prev = document.querySelector("#controls #prev");
 const play = document.querySelector("#controls #play");
+const prev = document.querySelector("#controls #prev");
 const next = document.querySelector("#controls #next");
 
 const player = new MusicPlayer(musicList);
@@ -21,5 +21,40 @@ function displayMusic(music) {
 }
 
 play.addEventListener("click", () => {
+    const isMusicPlay = container.classList.contains("playing");
+    isMusicPlay ? pauseMusic() : playMusic();
+});
+
+next.addEventListener("click", () => {
+    nextMusic();
+});
+
+prev.addEventListener("click", () => {
+    prevMusic();
+});
+
+function prevMusic() {
+    player.prev();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+function nextMusic() {
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+function pauseMusic() {
+    container.classList.remove("playing");
+    play.classList = "fa-solid fa-play";
+    audio.pause();
+}
+
+function playMusic() {
+    container.classList.add("playing");
+    play.classList = "fa-solid fa-pause";
     audio.play();
-} )
+}
